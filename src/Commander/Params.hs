24cbe79c1d503844e75d107e07464244181bb1cd
James Wilson <me@unbui.lt>
2016-03-06 17:36:25 +0000

    improve docs for Flag. rename and expose FromString type so others can extend Flag/Value support.

--- src/Commander/Params.hs
+++ src/Commander/Params.hs
@@ -15,8 +15,14 @@
     ScopedTypeVariables #-}
 
 module Commander.Params (
+
+    -- * Function Parameter Types
     Flag(..),
-    Value(..)
+    Value(..),
+
+    -- * Casting from String
+    FromString(..)
+
 ) where
 
 import Data.Proxy (Proxy(..))
@@ -28,17 +34,24 @@ import Commander.Commands (ToParam(..), ParamFlags(..), ParamHelp(..))
 -- require a flag. The type signature for this lists the flags that we
 -- want to match against, the associated help text, and the output type
 -- we want the flag to be cast to.
+--
+-- @Flag@s of @Maybe@ or @Bool@ types have special handling:
+-- if the flag doesnt exist for either of these types, we'll be
+-- handed back a @False@/@Nothing@ rather than get an error,
+-- else if the flag does exist we'll get back a @True@/@Just val@.
+-- @Bool@ flags are expected to be provided an empty string as the value;
+-- if you care about the value but want it to be optional, use @Maybe@.
 data Flag (flags :: [Symbol]) (help :: Symbol) a = Flag a
 
-instance FromText a => ToParam (Flag flags help (Maybe a)) where
-    toParam (Just str) = fmap (Flag . Just) (fromText str)
+instance FromString a => ToParam (Flag flags help (Maybe a)) where
+    toParam (Just str) = fmap (Flag . Just) (fromString str)
     toParam Nothing = Right (Flag Nothing)
 instance ToParam (Flag flags help Bool) where
     toParam (Just "") = Right (Flag True)
     toParam (Just _) = Left "boolean string does not expect to have a value"
     toParam Nothing = Right (Flag False)
-instance {-# OVERLAPPABLE #-} FromText a => ToParam (Flag flags help a) where
-    toParam (Just str) = fmap Flag (fromText str)
+instance {-# OVERLAPPABLE #-} FromString a => ToParam (Flag flags help a) where
+    toParam (Just str) = fmap Flag (fromString str)
     toParam Nothing = Left "flag expected but not found"
 instance KnownSymbols flags => ParamFlags (Flag flags help a) where
     paramFlags _ = Just $ symbolVals (Proxy :: Proxy flags)
@@ -51,28 +64,26 @@ instance KnownSymbol help => ParamHelp (Flag flags help a) where
 -- to.
 data Value (help :: Symbol) a = Value a
 
-instance FromText a => ToParam (Value help a) where
-    toParam (Just str) = fmap Value (fromText str)
+instance FromString a => ToParam (Value help a) where
+    toParam (Just str) = fmap Value (fromString str)
     toParam Nothing = Left "value expected but none found"
 instance ParamFlags (Value help a) where
     paramFlags _ = Nothing
 instance KnownSymbol help => ParamHelp (Value help a) where
     paramHelp _ = symbolVal (Proxy :: Proxy help)
 
---
--- Typeclass to extract details from flag/value type, or construct the
--- type from some string. This allows custom types to be used instead of
--- the above.
---
+-- | Typeclass used by 'Flag' and 'Value' to convert the provided string to
+-- the desired haskell type. Anything that satisfies @Read@ will satisfy this,
+-- but we can override the @Read@ behaviour as we see fit on a per type basis
+-- by explicitly implementing this.
+class FromString a where
+    fromString :: String -> Either String a
 
-class FromText a where
-    fromText :: String -> Either String a
+instance FromString String where
+    fromString = Right
 
-instance FromText String where
-    fromText = Right
-
-instance {-# OVERLAPPABLE #-} Read a => FromText a where
-    fromText str = case readMaybe str of
+instance {-# OVERLAPPABLE #-} Read a => FromString a where
+    fromString str = case readMaybe str of
         Nothing -> Left "string could not be cast to required type"
         Just a  -> Right a
 
